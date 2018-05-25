a9f90fb5e2f44ff842d88b12caf1c5ce2485907a
bodycoder <402392352@qq.com>
2018-05-25 17:52:44 +0800

    fix error for whitespace

--- AI_agent.py
+++ AI_agent.py
@@ -214,7 +214,7 @@ class stubAI:
         self.sb = data["table"]["smallBlind"]["amount"]
         for player in data["players"]:
             if player["playerName"] == self.playerName:
-				logging.info("remain chips:%s" % player["chips"])
+                logging.info("remain chips:%s" % player["chips"])
                 global totalReloadCount
                 self.reloadCount = totalReloadCount - player["reloadCount"]
 
